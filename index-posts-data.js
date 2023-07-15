3d6972870ed4852eac0fdb903b5a015862fb099a
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-07-15 22:09:04 +0700

    vault backup: 2023-07-15 22:09:04

--- index-posts-data.js
+++ index-posts-data.js
@@ -2,138 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const matter = require("gray-matter");
 const moment = require("moment");
-const { Octokit } = require("@octokit/rest");
-
-// Doesn't need API token since I only run this every hour.
-// May need to add token if I want to run this more often.
-const octokit = new Octokit({
-  auth: process.env.PAT,
-});
-const owner = "mbaharip";
-const repo = "mbaharip-blog-posts";
-const useLocal = true; // Set to true for using fs instead of octokit
-
-async function run() {
-  const timestampStart = Date.now();
-
-  const fetchBlogs = octokit.rest.repos.getContent({
-    owner,
-    repo,
-    path: "posts",
-  });
-  const fetchWorks = octokit.rest.repos.getContent({
-    owner,
-    repo,
-    path: "works",
-  });
-
-  const [blogsData, worksData] = await Promise.all([
-    fetchBlogs,
-    fetchWorks,
-  ]).then(([blogs, works]) => {
-    return [blogs.data, works.data];
-  });
-
-  const processBlogs = Promise.all(
-    blogsData.map(async (file) => {
-      if (file.type !== "file") return null;
-
-      const { data: fileData } = await octokit.rest.repos.getContent({
-        owner,
-        repo,
-        path: file.path,
-      });
-
-      const data = Buffer.from(fileData.content, "base64").toString("utf8");
-      const markdownData = matter(data);
-      let markdownContent = markdownData.content;
-      // check if markdownContent start with \n, if yes remove the first line break
-      if (markdownContent.startsWith("\n")) {
-        markdownContent = markdownContent.slice(1);
-      }
-
-      const summary = markdownContent
-        .replace(/!\[\[([^\]]+)\]\]/g, "")
-        .replace(/<[^>]*>?/gm, "")
-        .replace(/#+\s/g, "")
-        .split("\n")
-        .slice(0, 3) // Take 2 first lines, it use 3 because frontmatter content start with \n
-        .join(" ")
-        .trim();
-
-      return {
-        title: markdownData.data.title,
-        summary,
-        createdAt:
-          markdownData.data.createdAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        updatedAt:
-          markdownData.data.updatedAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        thumbnail: markdownData.data.thumbnail ?? null,
-        thumbnail_y: markdownData.data.thumbnail_y ?? 0.5,
-        thumbnail_x: markdownData.data.thumbnail_x ?? 0.5,
-        tags: markdownData.data.tags ?? [],
-        path: file.path.replace(".md", ""),
-      };
-    })
-  );
-  const processWorks = Promise.all(
-    worksData.map(async (file) => {
-      if (file.type !== "file") return null;
-
-      const { data: fileData } = await octokit.rest.repos.getContent({
-        owner,
-        repo,
-        path: file.path,
-      });
-
-      const data = Buffer.from(fileData.content, "base64").toString("utf8");
-      const markdownData = matter(data);
-      let markdownContent = markdownData.content;
-      // check if markdownContent start with \n, if yes remove the first line break
-      if (markdownContent.startsWith("\n")) {
-        markdownContent = markdownContent.slice(1);
-      }
-
-      const summary = markdownContent
-        .replace(/!\[\[([^\]]+)\]\]/g, "")
-        .replace(/<[^>]*>?/gm, "")
-        .replace(/#+\s/g, "")
-        .split("\n")
-        .slice(0, 3) // Take 2 first lines, it use 3 because frontmatter content start with \n
-        .join(" ")
-        .trim();
-
-      return {
-        title: markdownData.data.title,
-        summary,
-        createdAt:
-          markdownData.data.createdAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        updatedAt:
-          markdownData.data.updatedAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        thumbnail: markdownData.data.thumbnail ?? null,
-        thumbnail_y: markdownData.data.thumbnail_y ?? 0.5,
-        thumbnail_x: markdownData.data.thumbnail_x ?? 0.5,
-        tags: markdownData.data.tags ?? [],
-        path: file.path.replace(".md", ""),
-      };
-    })
-  );
-
-  const [blogs, works] = await Promise.all([processBlogs, processWorks]);
-
-  const dbPath = path.resolve(__dirname, "@db");
-  const blogsIndexPath = path.resolve(dbPath, "blogsIndex.json");
-  const worksIndexPath = path.resolve(dbPath, "worksIndex.json");
-  fs.writeFileSync(blogsIndexPath, JSON.stringify(blogs, null, 2));
-  fs.writeFileSync(worksIndexPath, JSON.stringify(works, null, 2));
-
-  console.log("Done");
-  console.log("Process time:", Date.now() - timestampStart, "ms");
-}
 
 async function runFs() {
   const timestampStart = Date.now();
@@ -176,7 +44,7 @@ async function runFs() {
       let thumbnail = markdownData.data.thumbnail ?? "/img/no-image.webp";
       if (/!\[\[([^\]]+)\]\]/g.test(thumbnail)) {
         thumbnail = thumbnail.match(/!\[\[([^\]]+)\]\]/)?.[1] ?? "logo.webp";
-        thumbnail = `/api/attachments/banner/${thumbnail}`;
+        thumbnail = `@attachments/banner/${thumbnail}`;
       } else {
         thumbnail = thumbnail.replace(/"/g, "").replace(/\\/g, "");
       }
@@ -231,7 +99,7 @@ async function runFs() {
       let thumbnail = markdownData.data.thumbnail ?? "/img/no-image.webp";
       if (/!\[\[([^\]]+)\]\]/g.test(thumbnail)) {
         thumbnail = thumbnail.match(/!\[\[([^\]]+)\]\]/)?.[1] ?? "logo.webp";
-        thumbnail = `/api/attachments/banner/${thumbnail}`;
+        thumbnail = `@attachments/banner/${thumbnail}`;
       } else {
         thumbnail = thumbnail.replace(/"/g, "").replace(/\\/g, "");
       }
@@ -277,14 +145,7 @@ async function runFs() {
   console.log("Process time:", Date.now() - timestampStart, "ms");
 }
 
-if (useLocal) {
-  runFs().catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
-} else {
-  run().catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
-}
+runFs().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
