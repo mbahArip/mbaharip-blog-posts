ffff365ba0d69a9f2463db83e0c3a02fd5d93eca
mbaharip <62494292+mbahArip@users.noreply.github.com>
2023-07-17 00:15:49 +0700

    Move processing function to outside.

---
+++ blogs/lorem-ipsum.md
@@ -0,0 +1,11 @@
+---
+title: Lorem ipsum
+thumbnail: "https://cdn.discordapp.com/attachments/256771512445566976/1130165925186899998/FB_IMG_1689522921844.jpg"
+thumbnail_x: 0.5
+thumbnail_y: 0.5
+tags:
+  - 
+createdAt: 2023-07-17T00:13:25+07:00
+updatedAt: 2023-07-17T00:14:16+07:00
+---
+Test url banner
--- index-posts-data.js
+++ index-posts-data.js
@@ -11,116 +11,8 @@ async function runFs() {
 
   const [blogsData, worksData] = await Promise.all([fetchBlogs, fetchWorks]);
 
-  const processBlogs = Promise.all(
-    blogsData.map(async (file) => {
-      const fileData = await fs.promises.readFile(
-        path.resolve(__dirname, "blogs", file),
-        "utf8"
-      );
-
-      const markdownData = matter(fileData);
-      let markdownContent = markdownData.content;
-      // check if markdownContent start with \n, if yes remove the first line break
-      if (markdownContent.startsWith("\n")) {
-        markdownContent = markdownContent.slice(1);
-      }
-
-      // markdown.data.tags if null return either null / [null]
-      // make it empty array if null
-      let tags = [];
-      if (Array.isArray(markdownData.data.tags) && markdownData.data.tags[0]) {
-        tags = markdownData.data.tags;
-      }
-
-      const summary = markdownContent
-        .replace(/!\[\[([^\]]+)\]\]/g, "")
-        .replace(/<[^>]*>?/gm, "")
-        .replace(/#+\s/g, "")
-        .split("\n")
-        .slice(0, 3) // Take 2 first lines, it use 3 because frontmatter content start with \n
-        .join(" ")
-        .trim();
-
-      let thumbnail = markdownData.data.thumbnail ?? "/img/banner.webp";
-      if (/!\[\[([^\]]+)\]\]/g.test(thumbnail)) {
-        thumbnail = thumbnail.match(/!\[\[([^\]]+)\]\]/)?.[1] ?? "logo.webp";
-        thumbnail = `@attachments/banner/${thumbnail}`;
-      } else {
-        thumbnail = thumbnail.replace(/"/g, "").replace(/\\/g, "");
-      }
-
-      return {
-        title: markdownData.data.title ?? file.replace(".md", "") ?? "untitled",
-        summary,
-        createdAt:
-          markdownData.data.createdAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        updatedAt:
-          markdownData.data.updatedAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        thumbnail: thumbnail ?? "/img/banner.webp",
-        thumbnail_y: markdownData.data.thumbnail_y ?? 0.5,
-        thumbnail_x: markdownData.data.thumbnail_x ?? 0.5,
-        tags,
-        path: `/blogs/${file.replace(".md", "")}`,
-      };
-    })
-  );
-  const processWorks = Promise.all(
-    worksData.map(async (file) => {
-      const fileData = await fs.promises.readFile(
-        path.resolve(__dirname, "works", file),
-        "utf8"
-      );
-
-      const markdownData = matter(fileData);
-      let markdownContent = markdownData.content;
-      // check if markdownContent start with \n, if yes remove the first line break
-      if (markdownContent.startsWith("\n")) {
-        markdownContent = markdownContent.slice(1);
-      }
-
-      // markdown.data.tags if null return either null / [null]
-      // make it empty array if null
-      let tags = [];
-      if (Array.isArray(markdownData.data.tags) && markdownData.data.tags[0]) {
-        tags = markdownData.data.tags;
-      }
-
-      const summary = markdownContent
-        .replace(/!\[\[([^\]]+)\]\]/g, "")
-        .replace(/<[^>]*>?/gm, "")
-        .replace(/#+\s/g, "")
-        .split("\n")
-        .slice(0, 3) // Take 2 first lines, it use 3 because frontmatter content start with \n
-        .join(" ")
-        .trim();
-
-      let thumbnail = markdownData.data.thumbnail ?? "/img/banner.webp";
-      if (/!\[\[([^\]]+)\]\]/g.test(thumbnail)) {
-        thumbnail = thumbnail.match(/!\[\[([^\]]+)\]\]/)?.[1] ?? "logo.webp";
-        thumbnail = `@attachments/banner/${thumbnail}`;
-      } else {
-        thumbnail = thumbnail.replace(/"/g, "").replace(/\\/g, "");
-      }
-
-      return {
-        title: markdownData.data.title ?? file.replace(".md", "") ?? "untitled",
-        summary,
-        createdAt:
-          markdownData.data.createdAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        updatedAt:
-          markdownData.data.updatedAt ??
-          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
-        thumbnail: thumbnail ?? "/img/banner.webp",
-        thumbnail_y: markdownData.data.thumbnail_y ?? 0.5,
-        thumbnail_x: markdownData.data.thumbnail_x ?? 0.5,
-        tags,
-        path: `/works/${file.replace(".md", "")}`,
-      };
-    })
-  );
+  const processBlogs = process(blogsData, "blogs");
+  const processWorks = process(worksData, "works");
 
   const [blogs, works] = await Promise.all([processBlogs, processWorks]).then(
     ([blogsData, worksData]) => {
@@ -143,6 +35,73 @@ async function runFs() {
 
   console.log("Done");
   console.log("Process time:", Date.now() - timestampStart, "ms");
+}
+
+function process(data, category) {
+  return Promise.all(
+    data.map(async (file) => {
+      const fileData = await fs.promises.readFile(
+        path.resolve(__dirname, category, file),
+        "utf8"
+      );
+      const markdownData = matter(fileData);
+
+      let content = markdownData.content;
+      // check if markdownContent start with \n, if yes remove the first line break
+      if (content.startsWith("\n")) {
+        content = content.slice(1);
+      }
+
+      // markdown.data.tags if null return either null / [null]
+      // make it empty array if null
+      let tags = [];
+      if (Array.isArray(markdownData.data.tags) && markdownData.data.tags[0]) {
+        tags = markdownData.data.tags;
+      }
+
+      // Create summary from markdown content
+      const summary = content
+        .replace(/!\[\[([^\]]+)\]\]/g, "")
+        .replace(/<[^>]*>?/gm, "")
+        .replace(/#+\s/g, "")
+        .split("\n")
+        .slice(0, 3) // Take 2 first lines, it use 3 because frontmatter content start with \n
+        .join(" ")
+        .trim();
+
+      let thumbnail = markdownData.data.thumbnail || "/img/banner.webp";
+      // Check if thumbnail is a wikilink image, if yes, get the image name
+      if (/!\[\[([^\]]+)\]\]/g.test(thumbnail)) {
+        thumbnail = thumbnail.match(/!\[\[([^\]]+)\]\]/)?.[1] ?? "logo.webp";
+        thumbnail = `@attachments/banner/${thumbnail}`;
+      } else {
+        thumbnail = thumbnail.replace(/"/g, "").replace(/\\/g, "");
+        // Check if it's a url, if yes add @url prefix
+        if (thumbnail.startsWith("http")) {
+          thumbnail = thumbnail.split("://")[1];
+          thumbnail = `@url/${thumbnail}`;
+        } else {
+          thumbnail = `@local${thumbnail}`;
+        }
+      }
+
+      return {
+        title: markdownData.data.title ?? file.replace(".md", "") ?? "untitled",
+        summary,
+        createdAt:
+          markdownData.data.createdAt ??
+          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
+        updatedAt:
+          markdownData.data.updatedAt ??
+          moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"),
+        thumbnail: thumbnail ?? "/img/banner.webp",
+        thumbnail_y: markdownData.data.thumbnail_y ?? 0.5,
+        thumbnail_x: markdownData.data.thumbnail_x ?? 0.5,
+        tags,
+        path: `/${category}/${file.replace(".md", "")}`,
+      };
+    })
+  );
 }
 
 runFs().catch((error) => {
